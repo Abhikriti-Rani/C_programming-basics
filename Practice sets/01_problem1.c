c8a5f8c7f46f7a082946bf79304b6b551eb34b20
Abhikriti Rani <abhikritrani@gmail.com>
2026-07-25 17:28:03 +0530

    Added area of rectangle programs

---
+++ Practice sets/01_problem1.c
@@ -0,0 +1,8 @@
+// Calculate area of rectangle using hard coded inputs
+#include<stdio.h>
+
+int main(){
+    int length=23;
+    int breadth=12;
+    printf("The area of rectangle is %d", length*breadth);     
+    return 0;}
