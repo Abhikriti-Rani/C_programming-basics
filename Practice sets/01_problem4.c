cef0f3b74ff9e87925f25f7864f4b112dec7e7fd
Abhikriti Rani <abhikritirani@gmail.com>
2026-07-27 01:07:42 +0530

    Conversion of Celsius to Fahrenheit

---
+++ Practice sets/01_problem4.c
@@ -0,0 +1,8 @@
+#include<stdio.h> // convert celsius to fahrenheit
+
+int main(){
+    float C= 38.6 , F;
+    F=(9.0/5.0)*C+32;
+    printf("The value of 38.6C in Fahrenheit is %f", F);
+    return 0;
+}
